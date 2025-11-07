10166e9ed22ec054547562af743cec1bef4b6725
adresz <tigerad97@gmail.com>
2025-11-07 11:04:51 +0100

    Initial commit

---
+++ src/main/java/com/example/javaeloadasbeadando/JavaEloadasBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaeloadasbeadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEloadasBeadandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEloadasBeadandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaeloadasbeadando/JavaEloadasBeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaeloadasbeadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEloadasBeadandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
